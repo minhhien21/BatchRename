acbc25d1adb410956e335a59d0acd33c6071be85
MinhHien21 <minhhien211299asdk@gmail.com>
2019-10-15 16:58:39 +0700

    một ít giao diện

--- BatchRename/BatchRename/MainWindow.xaml.cs
+++ BatchRename/BatchRename/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BatchRename
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
