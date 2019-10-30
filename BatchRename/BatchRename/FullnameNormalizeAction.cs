06405118ca7cc98ba5f882afcf87f2ae5549970f
hieuwu <43868345+hieuwu@users.noreply.github.com>
2019-10-31 03:03:35 +0700

    Thêm tính năng FullNameNormalizeAction

--- BatchRename/BatchRename/FullnameNormalizeAction.cs
+++ BatchRename/BatchRename/FullnameNormalizeAction.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace BatchRename
 {
     public class FullnameNormalizeArgs : StringArgs
     {
-
+        public string option { get; set; }
     }
     public class FullnameNormalizeAction : Action
     {
@@ -23,7 +24,20 @@ namespace BatchRename
 
         public override string Operate(string origin)
         {
-            throw new NotImplementedException();
+            var args = Args as FullnameNormalizeArgs;
+            var choosenOption = args.option;
+            var result = origin;
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            if (choosenOption == "No space at two endings")
+                return result.Trim();
+            else if (choosenOption == "Capitalize first letter")
+            {
+                result = textInfo.ToTitleCase(origin);
+                return result;
+            }
+            else
+                return result;
         }
     }
 
